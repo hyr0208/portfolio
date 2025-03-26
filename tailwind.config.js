7c9283888661ee5455cc13a9dc374ce943f214ee
yyyerin <hyr0208@naver.com>
2025-03-26 09:06:44 +0900

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // 리액트 파일들 경로 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
