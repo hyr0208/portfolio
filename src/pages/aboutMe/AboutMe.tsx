e8bb9621c0f74c513d0c8169217c59027818dce0
yyyerin <hyr0208@naver.com>
2025-03-27 12:28:32 +0900

    feat: add About me Page

---
+++ src/pages/aboutMe/AboutMe.tsx
@@ -0,0 +1,11 @@
+function AboutMe() {
+  return (
+    <div className="">
+      <div className=" relative h-screen bg-[#232323] ">
+        <h2 className="text-white text-6xl p-8">About me</h2>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,33 +1,12 @@
-import BackGroundImage from "../../assets/image/front_bg.jpg";
-import yerinImage from "../../assets/image/yerinImage.png";
+import AboutMe from "../aboutMe/AboutMe";
+import MainProfile from "../mainProfile/MainProfile";
 
 function MainPage() {
   return (
-    <div className="max-w-[1280px]">
-      <div className="relative w-screen h-screen">
-        <img
-          src={BackGroundImage}
-          alt=""
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+    <div className="">
+      <MainProfile />
 
-        <div className="absolute inset-0 text-center w-full pt-10">
-          <p className="text-[40px] md:text-[80px] text-white/80 font-normal tracking-tight leading-none">
-            Front-End Developer
-          </p>
-          <p className="absolute top-[8%] md:top-[10%] left-1/2 -translate-x-1/2 text-[26px] md:text-[50px] text-black font-normal leading-none">
-            프론트엔드 개발자
-          </p>
-        </div>
-
-        <div className="absolute inset-0 flex items-end justify-center 0">
-          <img
-            src={yerinImage}
-            alt=""
-            className="w-[500px] md:w-[650px] h-[700px] md:h-[900px] object-cover"
-          />
-        </div>
-      </div>
+      <AboutMe />
     </div>
   );
 }
