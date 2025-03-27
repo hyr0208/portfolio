4b6ee3b65f4706e2f38953ec2dd6de8462958252
yyyerin <hyr0208@naver.com>
2025-03-27 12:09:00 +0900

    refactor: 반응형 수정 및 yerinImage 추가

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,22 +1,31 @@
-import { imageData } from "../../constants/imageData";
+import BackGroundImage from "../../assets/image/front_bg.jpg";
+import yerinImage from "../../assets/image/yerinImage.png";
 
 function MainPage() {
   return (
     <div className="max-w-[1280px]">
-      <div className="object-cover w-screen relative">
+      <div className="relative w-screen h-screen">
         <img
-          src={imageData.BackGroundImage}
+          src={BackGroundImage}
           alt=""
-          className="w-screen h-screen"
+          className="absolute inset-0 w-full h-full object-cover"
         />
 
-        <div className="absolute bottom-0 left-0 top-0 text-center w-full pt-10">
-          <p className="md:text-[100px] text-[150px] text-white/80 font-normal tracking-tight leading-none">
+        <div className="absolute inset-0 text-center w-full pt-10">
+          <p className="text-[40px] md:text-[80px] text-white/80 font-normal tracking-tight leading-none">
             Front-End Developer
           </p>
-          <p className="absolute md:top-[20%] top-[30%] left-1/2 -translate-x-1/2 md:text-[50px] text-[70px] text-black font-light leading-none">
+          <p className="absolute top-[8%] md:top-[10%] left-1/2 -translate-x-1/2 text-[26px] md:text-[50px] text-black font-normal leading-none">
             프론트엔드 개발자
           </p>
+        </div>
+
+        <div className="absolute inset-0 flex items-end justify-center 0">
+          <img
+            src={yerinImage}
+            alt=""
+            className="w-[500px] md:w-[650px] h-[700px] md:h-[900px] object-cover"
+          />
         </div>
       </div>
     </div>
