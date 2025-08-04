d4615ef832a2ff5395f43edb0ab3184c8a0d7bb7
yyyerin <hyr0208@naver.com>
2025-08-04 17:10:14 +0900

    refactor: 내용 수정

--- src/constants/projectData.ts
+++ src/constants/projectData.ts
@@ -14,9 +14,18 @@ export const projects: Project[] = [
     id: 1,
     title: "라이더링크 랜딩페이지",
     description: "라이더링크 랜딩페이지 입니다.",
-    tags: ["팀", "앱", "사이드"],
+    tags: ["팀", "웹", "사이드"],
     category: "team",
     type: "publishing",
+    duration: "side"
+  },
+  {
+    id: 2,
+    title: "포트폴리오 사이트",
+    description: "저만의 포트폴리오를 만들기위해 제작한 사이트입니다.",
+    tags: ["솔로", "반응형", "포트폴리오"],
+    category: "single",
+    type: "portfolio",
     duration: "side"
   },
   
