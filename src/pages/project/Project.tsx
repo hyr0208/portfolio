997020a52bc21ea1c71827d4194ca982fab1e7ca
yyyerin <hyr0208@naver.com>
2025-08-05 16:24:43 +0900

    fix: 프로젝트 필터 수정

--- src/pages/project/Project.tsx
+++ src/pages/project/Project.tsx
@@ -20,6 +20,12 @@ const ProjectPage: React.FC = () => {
     setSelectedProject(null);
   };
 
+  // 필터링된 프로젝트 목록
+  const filteredProjects = projects.filter((project) => {
+    if (activeFilter === 'all') return true;
+    return project.category === activeFilter;
+  });
+
   return (
     <div className="min-h-screen bg-[#232323] text-white">
       <div className="container mx-auto px-6 py-8">
@@ -28,7 +34,7 @@ const ProjectPage: React.FC = () => {
           onFilterChange={setActiveFilter}
         />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((project) => (
+          {filteredProjects.map((project) => (
             <ProjectCard 
               key={project.id} 
               project={project} 
