84e86488929ee5fb6f7ff9850d435a484b713d4a
yyyerin <hyr0208@naver.com>
2025-11-21 12:59:25 +0900

    refactor: 이메일 보내기 툴팁 버튼 추가

--- src/pages/mainProfile/MainProfile.tsx
+++ src/pages/mainProfile/MainProfile.tsx
@@ -9,7 +9,7 @@ function MainProfile() {
   return (
     <div>
       <div className="relative h-screen overflow-hidden">
-        <div className="absolute md:top-14 top-5 md:left-20 left-5 z-10">
+        <div className="absolute md:top-14 top-5 md:left-20 left-5 z-10 flex gap-4 md:gap-6">
           <Tooltip text="GitHub로 이동" position="bottom">
             <a
               href="https://github.com/hyr0208"
@@ -18,6 +18,14 @@ function MainProfile() {
               rel="noopener noreferrer"
             >
               GitHub
+            </a>
+          </Tooltip>
+          <Tooltip text="이메일 보내기" position="bottom">
+            <a
+              href="hhyr0208@gmail.com"
+              className="relative flex gap-1 items-center flex-col z-20 font-normal py-1 px-2 transition-all ease-in-out duration-[0.3s] hover:text-[#fff] text-black after:content-[''] after:absolute after:bottom-[-2px] after:left-0 after:z-[-1] after:w-full after:h-1 after:bg-black after:block after:transition-all after:ease-in-out after:duration-[0.3s] hover:after:h-full"
+            >
+              Email
             </a>
           </Tooltip>
         </div>
