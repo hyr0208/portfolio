a58f67be82baae645347e0dc75aa41970fa7dfc3
yyyerin <hyr0208@naver.com>
2025-11-21 12:55:10 +0900

    refactor: 맨 위로 스크롤 버튼 첫페이지에서 안보이도록 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -6,16 +6,35 @@ import LastPage from "../lastPage/LastPage";
 // import Education from "../education/Education";
 import useScrollToTarget from "../../hooks/useScrollToTarget";
 import NavMenu from "../../components/NavMenu";
+import { useEffect, useState } from "react";
 
 function MainPage() {
   const careerSection = useScrollToTarget("Career");
   const projectSection = useScrollToTarget("Project");
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   const navTabs = [careerSection, projectSection];
 
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // 첫 페이지(뷰포트 높이)를 넘어갔을 때만 버튼 표시
+      const scrollPosition = window.scrollY;
+      const viewportHeight = window.innerHeight;
+
+      // 스크롤이 뷰포트 높이를 넘어갔을 때 버튼 표시
+      setShowScrollButton(scrollPosition > viewportHeight);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    // 초기 상태 확인
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <div className="bg-[#232323] text-white">
@@ -61,23 +80,25 @@ function MainPage() {
         </div>
       </div>
 
-      {/* 맨 위로 스크롤 버튼 */}
-      <button
-        className="fixed bottom-5 right-5 bg-white w-[50px] flex h-[50px] items-center justify-center font-bold rounded-full hover:opacity-80 shadow-lg"
-        onClick={scrollToTop}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="35"
-          height="35"
-          viewBox="0 0 24 26"
+      {/* 맨 위로 스크롤 버튼 - 첫 페이지를 넘어갔을 때만 표시 */}
+      {showScrollButton && (
+        <button
+          className="fixed bottom-5 right-5 bg-white w-[50px] flex h-[50px] items-center justify-center font-bold rounded-full hover:opacity-80 shadow-lg z-50 transition-opacity duration-300"
+          onClick={scrollToTop}
         >
-          <path
-            fill="black"
-            d="m5.84 15.41l5.66-5.66l5.66 5.66l-.71.7l-4.95-4.95l-4.95 4.95z"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 26"
+          >
+            <path
+              fill="black"
+              d="m5.84 15.41l5.66-5.66l5.66 5.66l-.71.7l-4.95-4.95l-4.95 4.95z"
+            />
+          </svg>
+        </button>
+      )}
     </div>
   );
 }
